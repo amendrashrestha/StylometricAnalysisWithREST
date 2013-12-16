f8fdeca8e6ace912070c1844842deaae05654996
Andreas Horndahl <horndahl@gmail.com>
2013-12-16 09:57:27 +0100

    IO prop: ah

--- StylometricAnalysisWithREST/src/main/java/com/board/analyze/IOHandler/IOProperties.java
+++ StylometricAnalysisWithREST/src/main/java/com/board/analyze/IOHandler/IOProperties.java
@@ -10,7 +10,9 @@ package com.board.analyze.IOHandler;
  */
 public class IOProperties {
 
-    public static final String INDIVIDUAL_USER_FILE_PATH = "C:\\Users\\ITE\\Documents\\NetBeansProjects\\StylometricAnalysisWithREST\\StylometricAnalysisWithREST\\src\\main\\resources\\dataFiles\\";
+    //public static final String INDIVIDUAL_USER_FILE_PATH = "C:\\Users\\ITE\\Documents\\NetBeansProjects\\StylometricAnalysisWithREST\\StylometricAnalysisWithREST\\src\\main\\resources\\dataFiles\\";
+    public static final String INDIVIDUAL_USER_FILE_PATH = "src\\main\\resources\\dataFiles\\";
+    
     public static final String USER_FILE_EXTENSION = ".txt";
     public static final String DATA_SEPERATOR = "This seperates users";
 }
